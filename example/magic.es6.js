fb85330ef75221e791523a7f7a5145e18a246b93
Denis Andrejew <da.colonel@gmail.com>
2015-06-09 13:34:29 +0100

    code simplified; would find handlers defined in parent templates, if the actual handler helpers were propagated down into children

--- example/magic.es6.js
+++ example/magic.es6.js
@@ -8,54 +8,67 @@ if (Meteor.isClient) {
       t.$('p').html('')
     },
   })
+
+  Template.parent.helpers({
+    children () {
+      return [1, 2]
+    }
+  })
+
+  Template.parent.events({
+    highlight (e, t) {
+      $(e.target).toggleClass('highlight')
+    }
+  })
+
+  // TODO this does not get matched yet because there is no `highlight` helper
+  // being defined in the child class yet, propagating down from the parent
+  // some messing around with data contexts would probably have to be done
+  // in order to implement such a thing
+  Template.child.events({
+    highlight (e, t) {
+      $(e.target).toggleClass('highlight')
+    }
+  })
 }
 
 // don't look, there's magic below!! ;-)
 
 if (Meteor.isClient) {
   function magicEventHelper (handlerName) {
-    let templateName = this.viewName
     return function () {
-      let f = (function (handlerName, templateName) {
+      let f = (function (handlerName) {
+        // make event
         let e = window.event
         jQuery.event.fix(e)
 
-        e.preventDefault()
-
+        // search through view hierarchy for appropriate handler
         let view = Blaze.getView(this)
-
-        // resolve global var name in templateName; could have just used eval(templateName)
-        let template = _.reduce(templateName.split('.'), (ret, part) => {
-          return ret && ret[part]
-        }, window)
-        while (view.template !== template) {
-          view = view.parentView
-          if (!view) {
-            console.log('no active view found for', template)
-            return
+        do {
+          let handler
+          if (_.find(view.template.__eventMaps, map => handler = map[handlerName])) {
+            return handler.call(view, e)
           }
-        }
-
-        let handler
-        _.find(view.template.__eventMaps, map => handler = map[handlerName])
-        if (!handler) {
-          return
-        }
-        return handler.call(view, e)
+        } while ((view = view.parentView))
+        // no handler found, nothing to do
       })
-      return '(' + f.toString() + ').bind(this)(\'' + handlerName + '\', \'' + templateName + '\')'
+      return '(' + f.toString() + ').bind(this)(\'' + handlerName + '\')'
     }
   }
 
+  // find all defined templates
+  let AllTemplates = _.values(Template).filter(t => t instanceof Blaze.Template && t.viewName.indexOf('Template._') === -1)
   // make helpers for all non-delegated/direct event handlers
-  Template.helloworld.__eventMaps.forEach(function (map) {
-    var helpers = _.reduce(map, (ret, evtFn, name) => {
-      if (name.indexOf(' ') !== -1) {
+  AllTemplates.forEach((TheTemplate) => {
+    TheTemplate.__eventMaps.forEach(function (map) {
+      let helpers = _.reduce(map, (ret, evtFn, name) => {
+        if (name.indexOf(' ') !== -1) {
+          return ret
+        }
+        ret[name] = magicEventHelper.call(this, name)
         return ret
-      }
-      ret[name] = magicEventHelper.call(this, name)
-      return ret
-    }, {})
-    this.helpers(helpers)
-  }.bind(Template.helloworld))
+      }, {})
+      this.helpers(helpers)
+    }.bind(TheTemplate))
+  })
 }
